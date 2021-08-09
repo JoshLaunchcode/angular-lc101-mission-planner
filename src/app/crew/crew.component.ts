31dc2afb1f1998dea792d57b9df55e032e7f21af
JoshLaunchcode <holo.sf0@gmail.com>
2021-08-09 18:24:54 -0500

    finished main studio

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -13,9 +13,27 @@ export class CrewComponent implements OnInit {
     {name: "Ellen Ochoa", firstMission: true}
   ];
 
+  memberBeingEdited: object = null;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  add(name: string, firstMission: string){
+    this.crew.push({name: name, firstMission: firstMission})
+  }
+
+  remove(crewMember: object){
+    this.crew.splice(this.crew.indexOf(crewMember), 1)
+  }
+
+  edit(member: object){
+    this.memberBeingEdited = member;
+  }
+
+  save(name: string, member: object){
+    member['name'] = name;
+    this.memberBeingEdited = null;
+  }
 }
